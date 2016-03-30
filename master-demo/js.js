0cfe9491b8c99bcfe9b138a24dab932310d1dd59
zigzag <447587073@qq.com>
2016-03-30 20:06:18 +0800

    kuaichengle

--- master-demo/js.js
+++ master-demo/js.js
@@ -54,29 +54,38 @@ var bindMove = (function() {
         var that = this;
         this.dom = dom;
 
-        var startX, startY, endX, endY, curX, curY;
+        //var startX, startY, endX, endY, curX, curY;
+        var startX, endX, curX , oldX;  
 
         this._start = function(e) {
-            var t = e.changedTouches[0];
-            startX = curX = t.clientX;
-            startY = curY = t.clientY;
+            var t = e.changedTouches[0]; // 获取当前 事件的
+            startX = curX = t.clientX;   //开始的坐标和 当前的坐标相等
+            //startY = curY = t.clientY;
             this.addEventListener('touchmove', that._move, false);
             this.addEventListener('touchend', that._end, false);
         };
 
         this._move = function(e) {
             var t = e.changedTouches[0];
-            endX = t.clientX;
-            endY = t.clientY;
-            callback(endX - curX, endY - curX, curX - startX, curY - startY);
-            curX = endX;
-            curY = endY;
+            endX = t.clientX;   // 此时为结束的坐标
+            oldX = this.style.left;
+            console.log(oldX);
+            //endY = t.clientY;
+            //callback(endX - curX, endY - curY, curX - startX, curY - startY);
+            callback(endX - curX, curX - startX);
+            curX = endX;        //此时当前的 坐标等于 结束时候的坐标
+            //curY = endY;
+            // endX - curX  =  判断鼠标左右 移动 
+            // curX - startX =  move 的距离
+            
         };
 
         this._end = function(e) {
             this.removeEventListener('touchmove', that._move);
             this.removeEventListener('touchend', that._end);
         };
+
+        
 
         this.dom = dom;
 
@@ -114,14 +123,45 @@ var bindMove = (function() {
         }
     });
 
-    var myMove = bindMove(el, function(x, y, maxX, maxY) {
-        console.log('moveing', x, y, maxX, maxY);
+    
+    // var myMove = bindMove(el, function(x, y,maxX, maxY) {
+    var myMove = bindMove(el, function(x, moveX) {
 
+        console.log('moveing', x, moveX);
+        //回调返回的二个参数 x > 0 鼠标往右   x <= 0  鼠标往左
+        //第二个参数 是 
+
+        //获取当前ul 的实际宽度
+        var ulWidth = el.offsetWidth;
+        //获取当前可视范围的宽度
+        var realWidth = document.body.clientWidth;
+        //当前可以移动的宽度范围
+        var canMove = ulWidth - realWidth;
+
+        //console.log(ulWidth,realWidth,canMove); //504 414 90 
          
-        el.style.transform = "translate3d(0px,0px,0px)";
+        //el.style.transform = "translate3d(0px,0px,0px)";
+         
 
-        if( y <= -150 ){
-            el.style.transform = "translate3d(0px,0px,0px)";
+        // if(  maxX <= 0 ){
+        //     if(maxX < canMove){
+        //         el.style.transform = "translate3d("+maxX+"px,0px,0px)";
+        //     }else{
+        //         el.style.transform = "translate3d("+canMove+"px,0px,0px)";
+        //     }
+            
+        // }
+        if(x>0){
+            //console.log('right');
+            if(el.style.left <0){
+                console.log(1);
+            }
+            //el.style.left = 0 +"px";
+            //el.style.left = moveX + "px";
+
+        }else if(x<=0){
+            console.log('left');
+            el.style.left = moveX + "px";
         }
 
     });
